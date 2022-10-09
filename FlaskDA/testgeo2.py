a0036c34f4e79147d21b5bd14f6f1885e1aca189
Chris Lirakis <clirakis@gmail.com>
2022-10-09 09:06:48 -0400

    testing how I use Geodesy.py (mine) which is a wrapper on pyproj. I may not need this. Also checking out PyGeodesy packge in testgeo2.py

--- FlaskDA/testgeo2.py
+++ FlaskDA/testgeo2.py
@@ -2,38 +2,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-
-from Geodetic import Geodetic as Geo
+from pygeodesy import utm
+from pygeodesy import points
 
 if __name__ == "__main__":
     """
     This is our main entry point. I wonder if I could define all the
     classes here.
     """
-    pGeo = Geo()
-    print(pGeo)
     # https://www.earthpoint.us/convert.aspx
     # results for default center should be
     # 18T 590033mE 4794728mN
     #
     # https://scienceweb.whoi.edu/marine/ndsf/cgi-bin/NDSFutility.cgi?form=0&from=LatLon&to=UTM
-    # 592648.4 4573593.9    Zone 18 CHECK
+    # 592648.4 4573593.9   Zone 18 CHECK
     #
-    # 08-Oct-22 - fixed this and all is good. 
+    # 08-Oct-22
     X0 =  592648.4
     Y0 = 4573593.9
-
     # Google Maps position for 217 Locust Ave
-    Lat = 41.308489
+    Lat =  41.308489
     Lon = -73.893027
-    X,Y = pGeo.ToXY(Lon, Lat)
-    print("X: " , X, " Y:", Y, " DX: ", X-X0, " DY: ", Y-Y0)
+    print('UTM ', utm)
+    print('Zone: ', utm.utmZoneBand5(Lat, Lon))
+    p = points.LatLon_(lat=Lat,lon=Lon)
+    print("Points: ", p)
+    u = utm.toUtm8(Lat, Lon)
+    print("UTM: ", u)
+    print("X: " , u.easting, " Y:", u.northing, " DX: ", u.easting-X0, " DY: ", u.northing-Y0)
 
     # and back.
-    LY, LX = pGeo.ToLL(X,Y)
-    print("Lat:", LY, " Lon:", LX)
+    #LY, LX = pGeo.ToLL(X,Y)
+    #print("Lat:", LY, " Lon:", LX)
 
     LatRI = 41.5
     LonRI = -71.3
-    az,R = pGeo.RangeBearing(Lon, Lat, LonRI, LatRI)
-    print("Azimuth: ", az," Range(m):", R)
+    #az,R = pGeo.RangeBearing(Lon, Lat, LonRI, LatRI)
+    #print("Azimuth: ", az," Range(m):", R)
