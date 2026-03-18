08888a9ada4f38691bea5fa1420004f74580d23b
Chris Lirakis <clirakis@gmail.com>
2026-03-18 14:37:42 -0400

    Hopefully this fixes the page issuee

--- Flask/PiDA.py
+++ Flask/PiDA.py
@@ -213,7 +213,7 @@ def filename():
     """
     if request.method == 'POST':
         COMM.Write("CF", 2)
-    return gps()
+    return render_template("DONE.html")
 
 @app.route('/marker', methods=['GET', 'POST'])
 def marker():
@@ -223,7 +223,7 @@ def marker():
     """
     if request.method == 'POST':
         COMM.Write("CM", 2)
-    return gps()
+    return render_template("DONE.html")
 
 @app.route('/testme')
 def testme():
